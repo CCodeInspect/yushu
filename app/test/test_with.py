cf060d13738c5d7c3c2cb20838972ed89f8c30d6
pauline <302869907@qq.com>
2023-01-09 16:10:49 +0800

    增加对于with这个上下文管理器的测试

---
+++ app/test/test_with.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: pauline.lee
+@contact: Pauline.Y.Li@outlook.com
+@site: https://github.com/llaichiyu/
+@file: test_with.py
+@time: 2023/1/9 15:09
+
+"""
+
+
+class A:
+    def __enter__(self):
+        print('进行占用资源')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_tb:
+            print("process exception")
+        else:
+            print("no exception")
+        print("释放资源")
+        return False
+
+    def query(self):
+        print("进行查询")
+
+
+try:
+    with A() as qqq:
+        # 1 / 0  # 异常
+        qqq.query()
+except Exception as ex:
+    pass
